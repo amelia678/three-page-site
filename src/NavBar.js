aa27a295bdb4b6b59a0fdd8c0cba65541df3ea36
Amelia Schulz <amelia.schulz@gmail.com>
2018-12-07 11:51:34 -0500

    sets up inital website outline

---
+++ src/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const NavBar = (props) => {
+    return (
+        <div>
+        <nav className="navigation-bar">
+          <a href="#">home</a>
+          <br></br>
+          <a href="#">cats</a>
+          <br></br>
+          <a href="#">about</a>
+        </nav>
+      </div>
+    )
+}
+
+
+export default NavBar;
---
+++ src/Webpage.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import NavBar from './NavBar';
+
+
+class Webpage extends Component {
+    constructor(props) {
+        super(props);
+        
+    }
+
+    render() {
+        return(
+        <NavBar />
+        )
+    }
+}
+
+export default Webpage;
